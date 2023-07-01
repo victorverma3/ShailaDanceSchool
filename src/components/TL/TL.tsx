8f94a39a0ccd0ed3dc208ee6fa0211c02a29a26a
victor <victor.verma@icloud.com>
2023-07-01 09:50:59 -0400

    created timeline component

---
+++ src/components/TL/TL.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TL.css";
+
+const TL = () => {
+  return <div>TL</div>;
+};
+
+export default TL;
--- src/pages/timeline/Timeline.tsx
+++ src/pages/timeline/Timeline.tsx
@@ -3,6 +3,26 @@ import "./Timeline.css";
 import Footer from "../../components/footer/Footer";
 
 const Timeline = () => {
+  const TLelements = [
+    {
+      id: 1,
+      year: "2023",
+      description: "placeholder",
+      image: "placeholder",
+    },
+    {
+      id: 2,
+      year: "2022",
+      description: "placeholder",
+      image: "placeholder",
+    },
+    {
+      id: 3,
+      year: "2021",
+      description: "placeholder",
+      image: "placeholder",
+    },
+  ];
   return (
     <>
       <div className="timelineContent">
