5910434c51e63b176435a6a46b2d8cadb5597383
victor <victor.verma@icloud.com>
2023-07-01 10:23:00 -0400

    working on timeline

--- src/pages/timeline/Timeline.tsx
+++ src/pages/timeline/Timeline.tsx
@@ -8,22 +8,108 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = () => {
-  const TLelements = [
+  const timelineElements = [
     {
       id: 1,
-      year: "2023",
+      title: "2023",
       description: "placeholder",
       image: "placeholder",
+      icon: "placeholder",
     },
     {
       id: 2,
-      year: "2022",
+      title: "2022",
       description: "placeholder",
       image: "placeholder",
+      icon: "placeholder",
     },
     {
       id: 3,
-      year: "2021",
+      title: "2021",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 4,
+      title: "2020",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 5,
+      title: "2019",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 6,
+      title: "2018",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 7,
+      title: "2017",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 8,
+      title: "2016",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 9,
+      title: "2015",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 10,
+      title: "2014",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 11,
+      title: "2013",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 12,
+      title: "2012",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 13,
+      title: "2011",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 14,
+      title: "2010",
+      description: "placeholder",
+      image: "placeholder",
+      icon: "placeholder",
+    },
+    {
+      id: 15,
+      title: "2009",
       description: "placeholder",
       image: "placeholder",
     },
@@ -32,6 +118,21 @@ const Timeline = () => {
     <>
       <div className="timelineContent">
         <h1 className="pageTitle">Timeline</h1>
+        <VerticalTimeline className="timelineComponent">
+          {timelineElements.map((element) => {
+            return (
+              <VerticalTimelineElement
+                key={element.id}
+                className="timelineElement"
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <img src={element.image} alt="image loading error"></img>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
       </div>
       <div className="bottom">
         <Footer />
