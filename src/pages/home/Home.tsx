f882937672f33267abf086f5b72a6aa02345b595
victor <victor.verma@icloud.com>
2023-07-02 18:33:15 -0400

    updated home testimonial layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,9 +2,31 @@ import React from "react";
 import "./Home.css";
 import Footer from "../../components/footer/Footer";
 import facebook from "../../images/facebook.png";
-import { Carousel } from "react-bootstrap";
+import stars from "../../images/stars.png";
 
 const Home = () => {
+  const homeTestimonials = [
+    {
+      rating: 5,
+      review:
+        "Shaila is a wonderful dance teacher, even for someone with no previous knowledge of Indian dancing. I enjoyed the class a lot. She is demanding, rigorous, firm, and very friendly at the same time. Her classes are a warm, supporting environment. Great experience.",
+    },
+    {
+      rating: 5,
+      review:
+        "Shaila is classically trained and very passionate dancer. Shaila is a great teacher and strives for perfection. I joined Shaila's bollywood dance lessons 4 years back and I love it. She is a amazing choreographer with an eye for details. Beautiful and graceful dancer and human being.",
+    },
+    {
+      rating: 5,
+      review:
+        "Shaila is an excellent dancer and an amazing teacher.I had the pleasure of being one of her first students when she opened the school and I still consider myself to be one of her student after many years, and hope to be one for many more to come.",
+    },
+    {
+      rating: 5,
+      review:
+        "Shaila Verma is a great dancer and most importantly, has a great teaching style. I enjoy the lessons in the studio as well as the group performances (BTW, I NEVER danced until the age of 55+, and now love to dance thanks to Shaila's encouragement and motivation).",
+    },
+  ];
   return (
     <>
       <div className="homeContent">
@@ -23,31 +45,20 @@ const Home = () => {
           <h3 className="homeSubtitle">Testimonials</h3>
         </a>
         <div className="testimonials">
-          <p className="quote">
-            "Shaila is a wonderful dance teacher, even for someone with no
-            previous knowledge of Indian dancing. I enjoyed the class a lot. She
-            is demanding, rigorous, firm, and very friendly at the same time.
-            Her classes are a warm, supporting environment. Great experience."
-          </p>
-          <p className="quote">
-            "Shaila is classically trained and very passionate dancer. Shaila is
-            a great teacher and strives for perfection. I joined Shaila's
-            bollywood dance lessons 4 years back and I love it. She is a amazing
-            choreographer with an eye for details. Beautiful and graceful dancer
-            and human being."
-          </p>
-          <p className="quote">
-            "Shaila is an excellent dancer and an amazing teacher.I had the
-            pleasure of being one of her first students when she opened the
-            school and I still consider myself to be one of her student after
-            many years, and hope to be one for many more to come."
-          </p>
-          <p className="quote">
-            "Shaila Verma is a great dancer and most importantly, has a great
-            teaching style. I enjoy the lessons in the studio as well as the
-            group performances (BTW, I NEVER danced until the age of 55+, and
-            now love to dance thanks to Shaila's encouragement and motivation)."
-          </p>
+          {homeTestimonials.map((element) => {
+            return (
+              <>
+                <div className="homeTestimonialElements">
+                  <img
+                    className="homeStars"
+                    src={stars}
+                    alt="image loading error"
+                  ></img>
+                  <p className="quote">{element.review}</p>
+                </div>{" "}
+              </>
+            );
+          })}
         </div>
         <br></br>
         <div className="homeInfo">
