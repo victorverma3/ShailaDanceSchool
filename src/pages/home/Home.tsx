0bf4645e66d812a2e9d618a2eba7dd1dbad07d8e
victor <victor.verma@icloud.com>
2023-07-02 12:14:56 -0400

    added testimonials to homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,12 +8,44 @@ const Home = () => {
     <>
       <div className="homeContent">
         <h1 className="homeTitle">Shaila School of Dance</h1>
-        <p className="tagline">Specializing in Bollywood dance since 2009</p>
+        <h3 className="homeSubtitle">
+          Specializing in Bollywood dance since 2009
+        </h3>
         <div className="homeImages">
           <img className="homePic" src="#" alt="image loading error"></img>
           <img className="homePic" src="#" alt="image loading error"></img>
           <img className="homePic" src="#" alt="image loading error"></img>
         </div>
+        <br></br>
+        <h3 className="homeSubtitle">Testimonials</h3>
+        <div className="testimonials">
+          <p className="quote">
+            "Shaila is a wonderful dance teacher, even for someone with no
+            previous knowledge of Indian dancing. I enjoyed the class a lot. She
+            is demanding, rigorous, firm, and very friendly at the same time.
+            Her classes are a warm, supporting environment. Great experience."
+          </p>
+          <p className="quote">
+            "Shaila is classically trained and very passionate dancer. Shaila is
+            a great teacher and strives for perfection. I joined Shaila's
+            bollywood dance lessons 4 years back and I love it. She is a amazing
+            choreographer with an eye for details. Beautiful and graceful dancer
+            and human being."
+          </p>
+          <p className="quote">
+            "Shaila is an excellent dancer and an amazing teacher.I had the
+            pleasure of being one of her first students when she opened the
+            school and I still consider myself to be one of her student after
+            many years, and hope to be one for many more to come."
+          </p>
+          <p className="quote">
+            "Shaila Verma is a great dancer and most importantly, has a great
+            teaching style. I enjoy the lessons in the studio as well as the
+            group performances (BTW, I NEVER danced until the age of 55+, and
+            now love to dance thanks to Shaila's encouragement and motivation)."
+          </p>
+        </div>
+        <br></br>
         <div className="homeInfo">
           <div className="homeInfoItem" id="homeLocation">
             <h1 className="homeInfoSubtitle">Westborough</h1>
@@ -29,6 +61,7 @@ const Home = () => {
               </a>
             </address>
           </div>
+          br
         </div>
         <a href="https://www.facebook.com/ShailaDanceSchool/" target="_blank">
           <img
