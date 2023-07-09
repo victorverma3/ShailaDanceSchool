467c101a4d9e91ea47a936dbc39c2f6cd4a18dd9
victor <victor.verma@icloud.com>
2023-07-09 18:51:05 -0400

    navbar collapse on select

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <>
       <div className="navContainer">
-        <Navbar className="nav" expand="lg" fixed="top">
+        <Navbar collapseOnSelect className="nav" expand="lg" fixed="top">
           <Container>
             <Navbar.Brand className="navBrand" href="/">
               Home
@@ -17,22 +17,22 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link>
+                <Nav.Link eventKey="1">
                   <Link className="navLink" to="/information">
                     Information
                   </Link>
                 </Nav.Link>
-                <Nav.Link>
+                <Nav.Link eventKey="2">
                   <Link className="navLink" to="/gallery">
                     Gallery
                   </Link>
                 </Nav.Link>
-                <Nav.Link>
+                <Nav.Link eventKey="3">
                   <Link className="navLink" to="/timeline">
                     Timeline
                   </Link>
                 </Nav.Link>
-                <Nav.Link>
+                <Nav.Link eventKey="4">
                   <Link className="navLink" to="/about">
                     About
                   </Link>
